0fe898c9ce8aad9a82235f8770b888449df50a1f
SupercuteYX <55016074+SupercuteYX@users.noreply.github.com>
2021-06-03 23:04:42 +0800

    Create Climbing the Leaderboard.py
    https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?isFullScreen=true

---
+++ Climbing the Leaderboard.py
@@ -0,0 +1,12 @@
+    #set()生成去掉duplicate之后的集合{}，list（）把集合{}转为list，sorted（）排序
+    rank = sorted(list(set(ranked)), reverse=True)
+    r = []
+            
+    for i in player:
+        # rank不为空，并且i>=rank list最后一个值
+        while rank and i >= rank[-1]: 
+            rank.pop()
+        # 注意！不在while循环内
+        r.append(len(rank)+1) 
+        
+    return r
